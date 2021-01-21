e79047c23b27cec217ce68139e8427792d8f8d92
choiHenry <wonderinghenry@gmail.com>
2021-01-21 18:06:02 +0900

    Modify gitignore

--- main.py
+++ main.py
@@ -4,10 +4,9 @@ myAPIKey = sys.argv[1]
 api = Dapi()
 
 api.apiKey = myAPIKey
-api.getCBDataAll()
 # api.saveGroupListData()
 # api.getCBDataCont('교보생명보험', 2019)
-
+api.getCBDataAll()
 # api.getCBData('HMM', '2020')
 
 
